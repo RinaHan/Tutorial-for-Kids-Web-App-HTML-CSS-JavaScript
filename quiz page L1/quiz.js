a9dac2e70e3c4dd55d7af4bf74284b8d645518a3
= <=>
2020-03-20 03:42:27 -0700

    I made Quiz gapes L2 and L3

--- quiz page L1/quiz.js
+++ quiz page L1/quiz.js
@@ -1,17 +1,19 @@
 var qnum = 0;
 var number_state = [
-    "Question 1",
-    "Question 2",
-    "Question 3",
-    "Question 4",
-    "Question 5"
+    "<a style=color:orange;>Leve 1</a> - Question 1",
+    "<a style=color:orange;>Leve 1</a> - Question 2",
+    "<a style=color:orange;>Leve 1</a> - Question 3",
+    "<a style=color:orange;>Leve 1</a> - Question 4",
+    "<a style=color:orange;>Leve 1</a> - Question 5",
+    "-Complete Level 1-"
 ]
 var image_state = [ 
     "url(imgs/img4.png)",
     "url(imgs/img1.png)",
     "url(imgs/img3.png)",
     "url(imgs/img2.png)",
-    "url(imgs/img5.png)"
+    "url(imgs/img5.png)",
+    "url(imgs/img6.jpg)",
 ]
 var question_state = [
     "You avoided having to engage with criticism by turning it back on the accuser - you answered criticism with criticism.",
@@ -19,17 +21,15 @@ var question_state = [
     "You misrepresented someone's argument to make it easier to attack.",
     "You said that if we allow A to happen, then Z will eventually happen too, therefore A should not happen.",
     "You said that because an authority thinks something, it must therefore be true.",
-    "<h1>Good Job!<br> You Can Go to Level 2<h1>"
+    "<h1>Good Job!<br>Let's Challenge Level 2!<h1>"
 ]
 var answer_state=[
-    "a: Tu Quoquey<br>"+"b: Appeal to Authorit<br>"+"c: Ad Hominem<br>"+"d: Slippery Slope<br>"+"e: strawman",
-    "a: Appeal to Authority<br>"+"b: To Quoque<br>"+"c: trawman<br>"+"d: Slippery Slope<br>"+"e: Ad Hominem",
-    "a: To Quoquey<br>"+"b: Appeal to Authorit<br>"+"c: Ad Hominem<br>"+"d: Slippery Slope<br>"+"e: strawman",
-    "a: To Quoquey<br>"+"b: Appeal to Authorit<br>"+"c: Ad Hominem<br>"+"d: Slippery Slope<br>"+"e: strawman",
-    "a: Appeal to Authority<br>"+"b: To Quoque<br>"+"c: trawman<br>"+"d: Slippery Slope<br>"+"e: Ad Hominem"
-
-
-
+    "a: Tu Quoquey<br>"+"b: Appeal to Authorit<br>"+"c: Ad Hominem<br>"+"d: Slippery Slope<br>"+"e: Strawman",
+    "a: Appeal to Authority<br>"+"b: To Quoque<br>"+"c: Strawman<br>"+"d: Slippery Slope<br>"+"e: Ad Hominem",
+    "a: To Quoquey<br>"+"b: Appeal to Authorit<br>"+"c: Ad Hominem<br>"+"d: Slippery Slope<br>"+"e: Strawman",
+    "a: To Quoquey<br>"+"b: Appeal to Authorit<br>"+"c: Ad Hominem<br>"+"d: Slippery Slope<br>"+"e: Strawman",
+    "a: Appeal to Authority<br>"+"b: To Quoque<br>"+"c: Strawman<br>"+"d: Slippery Slope<br>"+"e: Ad Hominem",
+    "You learned Appeal to Authority, To Quoque, Strawman, Slippery Slope, and Ad Hominem",
 ]
 var answercheck = [
     "a",
@@ -46,19 +46,18 @@ function start(){
     document.querySelector("#img_p").style.backgroundImage = image_state[qnum];
     document.querySelector("#question_p").innerHTML = question_state[qnum];
     document.querySelector("#answer").innerHTML = answer_state[qnum];
-    
 }
 function checkansweron(){
     var input = document.querySelector("#answerinput").value;
     console.log(input);
     if(input ===answercheck[qnum]){
-        document.querySelector("#respond").innerHTML="<h1>Right!</h1>";
+        document.querySelector("#respond").innerHTML="<h1 style=color:lightcoral;>That's right!</h1>";
         qnum++;
         document.querySelector("#question_n").innerHTML = number_state[qnum];
         document.querySelector("#img_p").style.backgroundImage = image_state[qnum];
         document.querySelector("#question_p").innerHTML = question_state[qnum];
         document.querySelector("#answer").innerHTML = answer_state[qnum];
 }else{
-    document.querySelector("#respond").innerHTML="<h1>Wrong, Let's try again!</h1>";
+    document.querySelector("#respond").innerHTML="<h1 style=color:red;>Wrong, Let's try again!</h1>";
 }
 }
